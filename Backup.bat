fccef14e589e67127eea82b880670f4821a38533
luktvpl <92756992+luktvpl@users.noreply.github.com>
2021-12-26 22:11:23 +0100

    Update Backup.bat

--- Backup.bat
+++ Backup.bat
@@ -1,6 +1,11 @@
 c:
 ::litera dysku backupów
 
+cd c:
+mkdir mcbackup
+::tworzy folder mcbackup jeśli go nie ma
+
+
 cd mcbackup
 :: wchodzi do folderu backupów
 
@@ -9,10 +14,12 @@ mkdir "backup%date:~7,2%_%date:~-10,2%_%date:~-4,4%-%time:~0,2%_%time:~3,2%"
 
 xcopy (wersja mc ponizej) c:\mcbackup\"backup%date:~7,2%_%date:~-10,2%_%date:~-4,4%-%time:~0,2%_%time:~3,2%" /E
 ::(wersja mc ponizej) W zależność od wersji piszemy
-::Java %appdata%/.minecraft/saves
+::Java %appdata%\.minecraft\saves
 ::Bedrock %USERPROFILE%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds
 
 pause
 ::zbędne ale pausuje po zakończeniu zadania żeby można było sprawdzić czy nie ma błędów
+
+
 
 ::wprzypadku gdy chcesz zmienić dysk na którym mają być zapisywane backupy podmieniasz c: na inną literę w pierwszej linijce oraz w 2 argumencie komendy xcopy
